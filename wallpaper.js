d538c3ea77c6fb76f7cfa33c14494247b2b27e44
Chris Regan <cregan22@gmail.com>
2017-10-16 01:32:52 -0700

    added image size checker to discard any less than square aspect ratio images

--- wallpaper.js
+++ wallpaper.js
@@ -26,7 +26,13 @@ var subreddits = [
 	"/r/EyeCandy",	// this one doesn't have much from imgur it seems
 	"/r/topwalls",
 	"/r/MinimalWallpaper",
-	"/r/GameWalls"
+	"/r/GameWalls",
+	"/r/ExposurePorn",
+	"/r/SkyPorn",
+	"/r/ImaginaryTechnology",
+	"/r/LightGraffiti",
+	"/r/futureporn",
+	"/r/lightpainting"
 ];
 
 // how to filter the subreddit - default is top posts from the past week
@@ -49,6 +55,7 @@ var path = "/home/chris/wallpapers/";
  ******************************/
 
 var https = require('https');
+var sizeOf = require('image-size');
 var fs = require('fs');
 var cheerio = require('cheerio');
 var options = {
@@ -73,8 +80,8 @@ function output(msg) {
 }
 
 // get HTML of path - the selected subreddit
-var getLinks = function(options) {
-	var req = https.get(options, function(response) {
+var getLinks = function(opt) {
+	var req = https.get(opt, function(response) {
 
 		// handle the response
 		var res_data = '';
@@ -128,19 +135,19 @@ var getLinks = function(options) {
 		    					// we don't have this one, download it
 								download(href, path+fileName, function(err){
 									if (err) {
-										output(href + " - ERROR: " + err);
+										output(opt.subreddit + " --- " + href + " - ERROR: " + err);
 									} else {
-										output(href + " - Download finished!");
+										output(opt.subreddit + " --- " + href + " - Download finished!");
 									}
 								});
 							} else {
 								// some other error here
-								output("ERROR: " + err.code);
+								output(opt.subreddit + " --- " + "ERROR: " + err.code);
 							}
 
 	    				} else {
 	    					// we have this file already, skip
-	    					output(fileName + " - already exists, skipping...");
+	    					output(opt.subreddit + " --- " + fileName + " - already exists, skipping...");
 	    				}
 	    			});
 	    		}
@@ -160,7 +167,19 @@ var download = function(url, dest, cb) {
 	var request = https.get(url, function(response) {
 		response.pipe(file);
 		file.on('finish', function() {
-			file.close(cb);  // close() is async, call cb after close completes.
+			file.close(function() {
+				// check the aspect ratio of this file, delete if less than square (like a phone wallpaper)
+				// obviously it would be better to check resolution and then delete, but it's fine for this script
+				sizeOf(dest, function (err, d) {
+					if (d.width / d.height < 0.9) {
+						// delete
+						fs.unlink(dest);
+						cb("Terrible aspect ratio");
+					} else {
+						cb();
+					}
+				});
+			});
 		});
 
 	}).on('error', function(err) {
@@ -185,6 +204,12 @@ if (!fs.existsSync(path)){
 }
 
 subreddits.forEach(function(value){
-	options.path = value + subredditFilter;
-	getLinks(options);
+	//options.path = value + subredditFilter;
+	//options.subreddit = value;
+	getLinks({
+		host: 'www.reddit.com',
+	    port: 443,
+		path: value + subredditFilter,
+		subreddit: value
+	});
 });
